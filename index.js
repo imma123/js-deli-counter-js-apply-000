b00b4c054ac896516bcdc17d1ce2c71d49e01f67
Miriam Friedler <friedleram@gmail.com>
2019-02-21 00:09:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function nowServing(katzline){
     return "There is nobody waiting to be served!"
   }
 }
+function currentLine (katzline) {
+  if (katzline.length > 0){
+    var message = "The line is currently: "
+    var i
+    for (i = 0; i < katzline.length; i++){
+       message += (i+1) + ". " + katzline[i]
+       if (i < (ketzline.length - 1)){ // add comma if there are more names in the array
+         message += ", "
+
+       }
+
+     }
+  }
+  else {
+    message = "The line is currently empty."
+  }
+  return message;
+}
